f19e51c36da47e2896507aaa2f96afb7cdee975d
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2022-12-27 03:19:10 +0200

    Finished Club Representative Page functionality except final requirement

--- milestone3/Club_representative.aspx
+++ milestone3/Club_representative.aspx
@@ -9,6 +9,46 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            Club Representative<br />
+            <br />
+            Club ID:
+            <asp:Label ID="clubID" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            Club Name:
+            <asp:Label ID="clubName" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            Club Location:
+            <asp:Label ID="clubLocation" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            <asp:GridView ID="upcomingMatchesTable" runat="server" AutoGenerateColumns="false">
+                 <Columns>
+        <asp:BoundField DataField="FirstClub" HeaderText="First Club" />
+        <asp:BoundField DataField="SecondClub" HeaderText="Second Club" />
+        <asp:BoundField DataField="start_time" HeaderText="Start Time" />
+        <asp:BoundField DataField="end_time" HeaderText="End Time" />
+        <asp:BoundField DataField="name" HeaderText="Stadium Name" />
+                 </Columns>
+            </asp:GridView>
+
+            <br />
+            Available Stadiums Starting :
+            <asp:TextBox ID="dateInput" runat="server" type="date"></asp:TextBox>
+
+            <asp:Button ID="Button1" runat="server" Text="View" OnClick="viewStadiums" style="width: 42px" />
+
+            <br />
+            <asp:GridView ID="stadiumsTable" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="name" HeaderText="Stadium Name" />
+                    <asp:BoundField DataField="location" HeaderText="Stadium Location" />
+                    <asp:BoundField DataField="capactiy" HeaderText="Stadium Capacity" />
+                </Columns>
+            </asp:GridView>
+            <br />
+
         </div>
     </form>
 </body>
--- milestone3/Club_representative.aspx.cs
+++ milestone3/Club_representative.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,6 +14,55 @@ namespace milestone3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            String connStr = WebConfigurationManager.ConnectionStrings["Milestone2#1"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            String infoCommand = "Select C.id, C.name, C.location FROM Club_representative cr INNER JOIN Club C ON (cr.Club_id = C.id) WHERE cr.Username = " +"'"+ Session["user"].ToString()+"'";
+            String clubNameCurrent="";
+            conn.Open();
+
+            SqlCommand clubInfo = new SqlCommand(infoCommand, conn);
+            
+            SqlDataReader infoReader = clubInfo.ExecuteReader();
+            while(infoReader.Read())
+            {
+               clubID.Text = infoReader["id"].ToString();
+               clubName.Text = infoReader["name"].ToString();
+               clubLocation.Text = infoReader["location"].ToString();
+               clubNameCurrent = infoReader["name"].ToString();
+            }
+            infoReader.Close();
+
+            String upcomingMatchesFunction = "SELECT * FROM upcomingMatchesOfClubNew(" +"'"+clubNameCurrent+"'"+")";
+
+            SqlDataAdapter viewUpcomingMatches = new SqlDataAdapter(upcomingMatchesFunction, conn);
+            DataTable dtbl = new DataTable();
+            viewUpcomingMatches.Fill(dtbl);
+            upcomingMatchesTable.DataSource = dtbl;
+            upcomingMatchesTable.DataBind();
+            
+            conn.Close();
+
+
+
+        }
+
+        protected void viewStadiums(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["Milestone2#1"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            conn.Open();
+            DateTime timeInput = DateTime.Parse(dateInput.Text);
+            String dateString = timeInput.ToString("yyyy/MM/dd");
+            
+            String availableStadiumsFunction = "SELECT * FROM viewAvailableStadiumsOn("+"'"+dateString+"'"+")";
+            SqlDataAdapter viewAvailableStadiums= new SqlDataAdapter(availableStadiumsFunction, conn);
+            DataTable dtbl = new DataTable();
+            viewAvailableStadiums.Fill(dtbl);
+            stadiumsTable.DataSource = dtbl;
+            stadiumsTable.DataBind();
+
 
         }
     }
